562c70b8cbf3947712b6a1d15ec8462aecc497dd
bertrand751979 <bertrandassinou@gmail.com>
2021-09-26 09:50:29 +0200

    first commit List Customer Spinner List Operation Repository Add Customer Repository calcul solde

--- app/src/main/java/com/example/mybankapplication/fragment/DisplayOperationFragment.java
+++ app/src/main/java/com/example/mybankapplication/fragment/DisplayOperationFragment.java
@@ -1,13 +1,17 @@
 package com.example.mybankapplication.fragment;
 
+import static android.content.ContentValues.TAG;
 import static com.example.mybankapplication.activities.MainActivity.MY_CHOICE_KEY;
+import static com.example.mybankapplication.activities.MainActivity.MY_CUSTOMER_KEY;
 import static com.example.mybankapplication.activities.MainActivity.MY_OPERATION_KEY;
 import static com.example.mybankapplication.fragment.EditOperationFragment.myOperationList;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
@@ -20,7 +24,10 @@ import com.example.mybankapplication.R;
 import com.example.mybankapplication.SharedPreferencesManager;
 import com.example.mybankapplication.adapter.AdapterOperation;
 import com.example.mybankapplication.models.Choice;
+import com.example.mybankapplication.models.Customer;
 import com.example.mybankapplication.models.Operation;
+import com.example.mybankapplication.models.Solde;
+import com.example.mybankapplication.repository.Repository;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
@@ -30,6 +37,7 @@ public class DisplayOperationFragment extends Fragment {
     private RecyclerView recyclerView;
     private AdapterOperation operationAdapter;
     private ArrayList<Operation> myOperationList=new ArrayList<>();
+    private Button btnSolde;
 
 
     @Override
@@ -50,6 +58,13 @@ public class DisplayOperationFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        btnSolde=view.findViewById(R.id.btnCalculSolde);
+        btnSolde.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         recyclerView = view.findViewById(R.id.recyclerViewListOperation);
         setViewItem();
         updateSolde();
@@ -60,11 +75,30 @@ public class DisplayOperationFragment extends Fragment {
         recyclerView.setAdapter(operationAdapter);
     }
     public void updateSolde() {
-        int count=0;
-        for (Operation operation : myOperationList) {
-            if (operation.getOperationAccountNumber().equalsIgnoreCase("RETRAIT")) {
-                count = count + 1;
+        double sommeRetrait=0;
+        double sommeDepot=0;
+        for (Customer customer : SharedPreferencesManager.getInstance
+                (DisplayOperationFragment.this.getContext()).getCustomer(MY_CUSTOMER_KEY)) {
+            Log.i(TAG,"Le num de compte"+customer.getCustomerAccountNumber());
+            for (Operation operation : SharedPreferencesManager.getInstance
+                    (DisplayOperationFragment.this.getContext()).getOperation(MY_OPERATION_KEY)){
+                if (customer.getCustomerAccountNumber().equalsIgnoreCase(operation.getOperationAccountNumber())) {
+                    Log.i(TAG,"Operation du numero de compte"+operation.getOperationAccountNumber());
+                    if(operation.getChoiceOperation().equalsIgnoreCase("Retrait")){
+                        sommeRetrait=customer.getCustomerAmount()-operation.getAmount();
+                        Log.i(TAG,"la somm est:"+sommeRetrait);
+                        customer.setCustomerAmount(sommeRetrait);
+                    }
+                    if(operation.getChoiceOperation().equalsIgnoreCase("Depot")){
+                        sommeDepot=customer.getCustomerAmount()+operation.getAmount();
+                        Log.i(TAG,"la somm est:"+sommeDepot);
+                        customer.setCustomerAmount(sommeDepot);
+                    }
+                }
+
             }
+
         }
-    }
-}
+
+    }//fin UpdateSolde()
+}//fin de Display
